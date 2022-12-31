1cfa14fc554e17bce5a3c6a347733156d441841f
saleem raza <sherohero33@gmail.com>
2022-12-31 17:37:52 +0500

    third exercise completed

---
+++ 3-tell-me-joke-game/tell-me-joke.js
@@ -0,0 +1,41 @@
+const joke = [
+    `One day YouTube, Twitter, and Facebook will join together and be called: YouTwitFace`,
+    `The past, present, and future walked into a bar. Things got a little tense.`,
+    `I like jokes about stationery, but rulers are where I draw the line.`,
+    `There should be confetti in tires, so it’s still an okay day when there is a blow-out. `,
+    `Bread is a lot like the sun. It rises in the yeast and sets in the waist. `,
+    `Geology rocks, but geography is where it’s at. `,
+    `70% of the earth is water, and virtually none of it is carbonated. So the earth is, in fact, flat.`,
+    `Just found out the company that produces yardsticks won’t be making them any longer. `,
+    `Went to the doctor with a suspicious-looking mole. He said they all look that way, and I should have left him in the garden. `,
+    `Engineers have made a car that can run on mint. Hopefully, they can make buses and trains run on thyme. `,
+    `What kind of car does an egg drive? A Yolksvagen.  `,
+    `It’s called gross pay because it’s disgusting to see how much money you would have made before taxes. `,
+    `Bravely killed a bug at home. Then realized it was a piece of lint. `,
+    `Did you hear about the corduroy pillow? It’s making headlines. `,
+    `Plagiarism: Getting into trouble for something you didn’t do.`,
+    `Time flies like an arrow. Fruit flies like a banana. -Groucho Marx`,
+    `I was going to tell a carpentry joke, but I couldn’t find any of that woodwork. `,
+    `An Irishman walks out of a bar. `,
+    `Velcro is a complete ripoff.`,
+    `Learn to spell… AutoCorrect isn’t always write. `,
+    `The fact that Head & Shoulders doesn’t have a body wash called  ‘Knees & Toes’ disappoints me. `,
+    `There are two kinds of people in the world: those who can extrapolate from incomplete data. `,
+    `I walked past a farm, and a sign said, “Duck, eggs.” I thought that was an unnecessary comma. And then it hit me.`,
+    `What’s the difference between Black Eyed Peas and Chick Peas? Black Eyed Peas can sing us a song. Chick Peas can hummus one. `,
+    `What’s Forrest Gump’s password. 1Forrest1.`,
+    `Why is cold water so insecure? It’s never been called hot. `,
+    `I sympathize with batteries. I’m not included in anything either.`,
+    `I like what mechanics wear…overall.`,
+    `Did you know that Davy Crockett had three ears? His left ear, his right ear, and his wild frontier.`,
+    `What’s a foot long and slippery? A slipper.`,
+    `Doctor: I’m sorry, but we had to remove your colon. Me Why?`
+]
+
+const randomJoke=Math.floor(Math.random()*joke.length);
+// console.log(joke[randomJoke]);
+jokes.innerHTML=joke[randomJoke];
+
+
+
+
